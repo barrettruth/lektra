cc94aff5c7f04bef4df584a4d8c669a5a87cd80d
Dheeraj Vittal Shenoy <dheerajshenoy22@gmail.com>
2026-03-06 00:26:50 +0100

    Set NoCache for PixmapItem

--- src/GraphicsPixmapItem.hpp
+++ src/GraphicsPixmapItem.hpp
@@ -1,7 +1,17 @@
 #pragma once
 
 #include <QGraphicsPixmapItem>
+#include <QImage>
+#include <QPixmap>
 
 class GraphicsPixmapItem : public QGraphicsPixmapItem
 {
+public:
+    GraphicsPixmapItem(QGraphicsItem *parent = nullptr)
+        : QGraphicsPixmapItem(parent)
+    {
+        // NoCache is appropriate for OpenGL backend as the pixmap
+        // is already a GPU texture
+        setCacheMode(QGraphicsItem::NoCache);
+    }
 };
