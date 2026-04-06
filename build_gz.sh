5569dc567e2d6cd5d31200a2202eb3ca935a2a47
Dheeraj Vittal Shenoy <dheerajshenoy22@gmail.com>
2026-04-06 15:21:17 +0200

    Update build_gz.sh script to show version

--- build_gz.sh
+++ build_gz.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 VERSION=$(grep -m1 'project(' CMakeLists.txt | grep -oP 'VERSION \K[0-9]+\.[0-9]+\.[0-9]+')
+echo "Building GZ file for Lektra version: $VERSION"
 DESTDIR="$PWD/pkg" cmake --install build
 tar -czvf lektra-${VERSION}-x86_64.tar.gz -C pkg .
